3c8055678c31e219fa866dec0e323eeb9fbff908
Ghusn-Mhsen <ghusn.mh.ite@gmail.com>
2022-05-12 10:49:46 +0200

    new

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -4,13 +4,13 @@ class Assets {
 
   static const String academyImagesResultIcon = 'images/resultIcon.svg';
   static const String images1 = 'images/1.jpg';
-  static const String imagesArt-studies = 'images/art-studies.svg';
+
   static const String imagesBgLogin = 'images/bg_login.png';
   static const String imagesButtonBg = 'images/button_bg.png';
   static const String imagesCollage = 'images/collage.png';
   static const String imagesCollages = 'images/collages.svg';
   static const String imagesConnection = 'images/connection.svg';
-  static const String imagesContact-us = 'images/contact-us.svg';
+
   static const String imagesDate = 'images/date.svg';
   static const String imagesDrawer = 'images/drawer.png';
   static const String imagesEducation = 'images/education.svg';
@@ -25,13 +25,12 @@ class Assets {
   static const String imagesLogoUp = 'images/logoUp.png';
   static const String imagesMenu = 'images/menu.svg';
   static const String imagesMicroscope = 'images/microscope.svg';
-  static const String imagesOnline-course (1) = 'images/online-course (1).svg';
-  static const String imagesOnline-education = 'images/online-education.svg';
+
+
   static const String imagesPharmacy = 'images/pharmacy.svg';
-  static const String imagesPolitical-science = 'images/political-science.svg';
+
   static const String imagesPortfolio = 'images/portfolio.svg';
-  static const String imagesPost-it = 'images/post-it.svg';
-  static const String imagesQuestion-mark = 'images/question-mark.svg';
+
   static const String imagesQuran = 'images/quran.svg';
   static const String imagesResult = 'images/result.svg';
   static const String imagesResultIcon = 'images/resultIcon.png';
@@ -42,6 +41,6 @@ class Assets {
   static const String imagesTimeEnd = 'images/timeEnd.svg';
   static const String imagesUnvirsites = 'images/unvirsites.svg';
   static const String imagesUser = 'images/user.svg';
-  static const String imagesUser (1) = 'images/user (1).svg';
+
 
 }
